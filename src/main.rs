89e71a3e69da62da51641c326e34661c25c35a98
Joshua Hopwood <josh.hopwood@ansys.com>
2023-12-28 18:01:00 -0600

    Add game display https://github.com/JHops881/rust-game/issues/6

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
-fn main() {
-    println!("Everything is awesome!");
-    println!("Everything is cool when you are part of a team!")
+use macroquad::prelude::*;
+
+#[macroquad::main("BasicShapes")]
+async fn main() {
+    
+    set_fullscreen(true);
+    loop {
+        // Clear screen
+        clear_background(BLACK);
+        
+        // Sample text
+        draw_text("rust-game", screen_width() / 2_f32, screen_height() / 2_f32, 12_f32, WHITE);
+
+        // Allow exiting
+        if is_key_down(KeyCode::Escape) {
+            std::process::exit(0);
+        }
+
+        next_frame().await
+    }
 }
+
+
