d45f9d8b711f189d576b28cde1eb9202e343eaea
Joshua Hopwood <josh.hopwood@ansys.com>
2023-12-29 11:54:18 -0600

    In progress

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,58 @@
 pub mod tiles;
 
-use ::rand::thread_rng;
-use ::rand::RngCore;
 use macroquad::prelude::*;
-use tiles::TileSet;
 use tiles::get_tile_set;
+use tiles::{gen_map_chunk, draw_map_chunk};
 
-#[macroquad::main("BasicShapes")]
+
+/// Returns the configuration settings for the Macroquad window
+/// see https://docs.rs/macroquad/latest/macroquad/attr.main.html
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Rust Game".to_owned(),
+        window_width: 640,
+        window_height: 480,
+        fullscreen: false,
+        ..Default::default()
+    }
+}
+
+/// Main function
+#[macroquad::main(window_conf)]
 async fn main() {
-
     let _tiles = get_tile_set();
-    let map = create_example_map();
+    
+    let map = gen_map_chunk(0.75);
 
-    set_fullscreen(true);
+    let mut looking_at: Vec2 = vec2(0., 0.);
+    let mut looking_zoom = 1_f32;
+
     loop {
         // Clear screen
         clear_background(BLACK);
-        
+
         // sample drawing of tiles
-        draw_map(&map, &_tiles);
+        draw_map_chunk(&map, &_tiles, &looking_at, &looking_zoom);
 
         // Allow exiting
         if is_key_down(KeyCode::Escape) {
             std::process::exit(0);
+        }
+
+        if is_mouse_button_down(MouseButton::Left) {
+            looking_at = mouse_delta_position() + looking_at;
+        } else {
+            // Discard the diff to avoid skipping around. This SHOULDN'T be
+            // necessary...
+            mouse_delta_position();
+        }
+
+        if mouse_wheel().1 > 0.1 {
+            looking_zoom = looking_zoom * 2.;
+        }
+
+        if mouse_wheel().1 < 0.0 {
+            looking_zoom = looking_zoom / 2.;
         }
 
         next_frame().await
@@ -30,57 +60,4 @@ async fn main() {
 }
 
 
-fn create_example_map() -> [bool; 100] {
-    let mut retval = [false; 100];
-    let mut _rng = thread_rng();
-    for i in 0..100 {
-        retval[i] = (_rng.next_u32() & 0x11) > 0;
-    }
-    retval
-}
-
-fn draw_map(map: &[bool; 100], _tiles: &TileSet) {
-    let params = DrawTextureParams {
-        dest_size: Some(vec2(100., 100.)),
-        ..Default::default()
-    };
-
-    for i in 0..100 {
-
-        if !map[i] {
-            continue;
-        }
-        let x: f32 = (i % 10) as f32 * 100. + 100.;
-        let y: f32 = (i / 10) as f32 * 100. + 100.;
-
-        let on_left_edge = i % 10 == 0;
-        let on_right_edge = i % 10 == 9;
-        let on_top_edge = i < 10;
-        let on_bot_edge = 89 < i;
-
-        let left_filled = on_left_edge || map[i-1];
-        let right_filled = on_right_edge || map[i+1];
-        let top_filled = on_top_edge || map[i-10];
-        let bot_filled = on_bot_edge || map[i+10];
-
-        match (left_filled, right_filled, top_filled, bot_filled) {
-            (true, true, true, true) => draw_texture_ex(&_tiles.center, x, y, WHITE, params.clone()),
-            (true, true, true, false) => draw_texture_ex(&_tiles.side_bottom, x, y, WHITE, params.clone()),
-            (true, true, false, true) => draw_texture_ex(&_tiles.side_top, x, y, WHITE, params.clone()),
-            (true, true, false, false) => draw_texture_ex(&_tiles.straight_left_right, x, y, WHITE, params.clone()),
-            (true, false, true, true) => draw_texture_ex(&_tiles.side_right , x, y, WHITE, params.clone()),
-            (true, false, true, false) => draw_texture_ex(&_tiles.corner_bottom_right, x, y, WHITE, params.clone()),
-            (true, false, false, true) => draw_texture_ex(&_tiles.corner_top_right, x, y, WHITE, params.clone()),
-            (true, false, false, false) => draw_texture_ex(&_tiles.peninsula_left, x, y, WHITE, params.clone()),
-            (false, true, true, true) => draw_texture_ex(&_tiles.side_left, x, y, WHITE, params.clone()),
-            (false, true, true, false) => draw_texture_ex(&_tiles.corner_bottom_left, x, y, WHITE, params.clone()),
-            (false, true, false, true) => draw_texture_ex(&_tiles.corner_top_left, x, y, WHITE, params.clone()),
-            (false, true, false, false) => draw_texture_ex(&_tiles.peninsula_right, x, y, WHITE, params.clone()),
-            (false, false, true, true) => draw_texture_ex(&_tiles.straight_top_bottom, x, y, WHITE, params.clone()),
-            (false, false, true, false) => draw_texture_ex(&_tiles.peninsula_top, x, y, WHITE, params.clone()),
-            (false, false, false, true) => draw_texture_ex(&_tiles.peninsula_bottom, x, y, WHITE, params.clone()),
-            (false, false, false, false) => (),
-        }
-    }
-}
 
--- src/tiles.rs
+++ src/tiles.rs
@@ -1,8 +1,15 @@
+use ::rand::rngs::ThreadRng;
+use ::rand::RngCore;
 use macroquad::prelude::*;
 
+/// The native width and height of a tile
+pub const TILE_DIM: u32 = 16;
+
 const CENTER_TILE_BYTES: &'static [u8; 155] = include_bytes!("../assets/tiles/center.png");
-const CORNER_BOTTOM_LEFT: &'static [u8; 196] = include_bytes!("../assets/tiles/corner-bottom-left.png");
-const CORNER_BOTTOM_RIGHT: &'static [u8; 184] = include_bytes!("../assets/tiles/corner-bottom-right.png");
+const CORNER_BOTTOM_LEFT: &'static [u8; 196] =
+    include_bytes!("../assets/tiles/corner-bottom-left.png");
+const CORNER_BOTTOM_RIGHT: &'static [u8; 184] =
+    include_bytes!("../assets/tiles/corner-bottom-right.png");
 const CORNER_TOP_LEFT: &'static [u8; 186] = include_bytes!("../assets/tiles/corner-top-left.png");
 const CORNER_TOP_RIGHT: &'static [u8; 207] = include_bytes!("../assets/tiles/corner-top-right.png");
 const PENINSULA_BOTTOM: &'static [u8; 235] = include_bytes!("../assets/tiles/peninsula-bottom.png");
@@ -13,8 +20,10 @@ const SIDE_BOTTOM: &'static [u8; 171] = include_bytes!("../assets/tiles/side-bot
 const SIDE_LEFT: &'static [u8; 181] = include_bytes!("../assets/tiles/side-left.png");
 const SIDE_RIGHT: &'static [u8; 129] = include_bytes!("../assets/tiles/side-right.png");
 const SIDE_TOP: &'static [u8; 186] = include_bytes!("../assets/tiles/side-top.png");
-const STRAIGHT_LEFT_RIGHT: &'static [u8; 200] = include_bytes!("../assets/tiles/straight-left-right.png");
-const STRAIGHT_TOP_BOTTOM: &'static [u8; 223] = include_bytes!("../assets/tiles/straight-top-bottom.png");
+const STRAIGHT_LEFT_RIGHT: &'static [u8; 200] =
+    include_bytes!("../assets/tiles/straight-left-right.png");
+const STRAIGHT_TOP_BOTTOM: &'static [u8; 223] =
+    include_bytes!("../assets/tiles/straight-top-bottom.png");
 
 pub struct TileSet {
     pub center: Texture2D,
@@ -28,11 +37,12 @@ pub struct TileSet {
     pub peninsula_top: Texture2D,
     pub side_bottom: Texture2D,
     pub side_left: Texture2D,
-    pub side_right : Texture2D,
+    pub side_right: Texture2D,
     pub side_top: Texture2D,
     pub straight_left_right: Texture2D,
     pub straight_top_bottom: Texture2D,
 }
+
 
 pub fn get_tile_set() -> TileSet {
     let png = Some(ImageFormat::Png);
@@ -52,5 +62,134 @@ pub fn get_tile_set() -> TileSet {
         side_top: Texture2D::from_file_with_format(SIDE_TOP, png),
         straight_left_right: Texture2D::from_file_with_format(STRAIGHT_LEFT_RIGHT, png),
         straight_top_bottom: Texture2D::from_file_with_format(STRAIGHT_TOP_BOTTOM, png),
+    }
+}
+
+/// Total tiles in a map chunk
+const CHUNK_SIZE: usize = 100;
+
+/// # Arguments
+///
+/// * `p` - The probability that a given tile will be `true`
+///
+/// # Returns
+///
+/// A section of the map of size `CHUNK_SIZE`. For now, just a bunch of `true`s
+/// and `false`s.
+pub fn gen_map_chunk(p: f32) -> [bool; CHUNK_SIZE] {
+    let mut map_chunk: [bool; CHUNK_SIZE] = [false; CHUNK_SIZE];
+    let mut random: ThreadRng = ThreadRng::default();
+
+    // If below the threshold, tile is true
+    let threshold = ((p as f64) * (std::u32::MAX as f64)).trunc() as u32;
+
+    for i in 0..CHUNK_SIZE {
+        map_chunk[i] = random.next_u32() < threshold;
+    }
+    return map_chunk;
+}
+
+/// Draw a section of the map to the screen
+///
+/// # Arguments
+///
+/// * `chunk` - The chunk to draw
+/// * `_tiles` - The tileset to draw with
+/// * `looking_at` - Where the camera is looking
+/// * `looking_zoom` - How zoomed the camera is. Smaller zoom value means
+///     more zoomed in.
+pub fn draw_map_chunk(
+    chunk: &[bool; CHUNK_SIZE],
+    _tiles: &TileSet,
+    looking_at: &Vec2,
+    looking_zoom: &f32,
+) {
+    // Scale tile dimensions by zoom level
+    let zoomed_tile_dim: f32 = TILE_DIM as f32 * looking_zoom;
+    let draw_params: DrawTextureParams = DrawTextureParams {
+        dest_size: Some(Vec2 {
+            x: zoomed_tile_dim,
+            y: zoomed_tile_dim,
+        }),
+        ..Default::default()
+    };
+
+    for i in 0..CHUNK_SIZE {
+        // If there is no tile, do not draw
+        if !chunk[i] {
+            continue;
+        }
+
+        // Position tile
+        let x: f32 = (i % 10) as f32 * zoomed_tile_dim - looking_at.x;
+        let y: f32 = (i / 10) as f32 * zoomed_tile_dim - looking_at.y;
+
+        // Detect if on an edge or adjacent to other tiles
+        let left_adjacent: bool = (i % 10 == 0) || chunk[i - 1];
+        let right_adjacent: bool = (i % 10 == 9) || chunk[i + 1];
+        let top_adjacent: bool = (i < 10) || chunk[i - 10];
+        let bot_adjacent: bool = (89 < i) || chunk[i + 10];
+
+        // Draw the right tile sprite for the given adjacency
+        match (left_adjacent, right_adjacent, top_adjacent, bot_adjacent) {
+            (true, true, true, true) => {
+                draw_texture_ex(&_tiles.center, x, y, WHITE, draw_params.clone())
+            }
+            (true, true, true, false) => {
+                draw_texture_ex(&_tiles.side_bottom, x, y, WHITE, draw_params.clone())
+            }
+            (true, true, false, true) => {
+                draw_texture_ex(&_tiles.side_top, x, y, WHITE, draw_params.clone())
+            }
+            (true, true, false, false) => draw_texture_ex(
+                &_tiles.straight_left_right,
+                x,
+                y,
+                WHITE,
+                draw_params.clone(),
+            ),
+            (true, false, true, true) => {
+                draw_texture_ex(&_tiles.side_right, x, y, WHITE, draw_params.clone())
+            }
+            (true, false, true, false) => draw_texture_ex(
+                &_tiles.corner_bottom_right,
+                x,
+                y,
+                WHITE,
+                draw_params.clone(),
+            ),
+            (true, false, false, true) => {
+                draw_texture_ex(&_tiles.corner_top_right, x, y, WHITE, draw_params.clone())
+            }
+            (true, false, false, false) => {
+                draw_texture_ex(&_tiles.peninsula_left, x, y, WHITE, draw_params.clone())
+            }
+            (false, true, true, true) => {
+                draw_texture_ex(&_tiles.side_left, x, y, WHITE, draw_params.clone())
+            }
+            (false, true, true, false) => {
+                draw_texture_ex(&_tiles.corner_bottom_left, x, y, WHITE, draw_params.clone())
+            }
+            (false, true, false, true) => {
+                draw_texture_ex(&_tiles.corner_top_left, x, y, WHITE, draw_params.clone())
+            }
+            (false, true, false, false) => {
+                draw_texture_ex(&_tiles.peninsula_right, x, y, WHITE, draw_params.clone())
+            }
+            (false, false, true, true) => draw_texture_ex(
+                &_tiles.straight_top_bottom,
+                x,
+                y,
+                WHITE,
+                draw_params.clone(),
+            ),
+            (false, false, true, false) => {
+                draw_texture_ex(&_tiles.peninsula_top, x, y, WHITE, draw_params.clone())
+            }
+            (false, false, false, true) => {
+                draw_texture_ex(&_tiles.peninsula_bottom, x, y, WHITE, draw_params.clone())
+            }
+            (false, false, false, false) => (),
+        }
     }
 }
