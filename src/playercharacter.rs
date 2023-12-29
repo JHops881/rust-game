1427430b2855d70539770c6763b2ae262e375e12
Joseph Hopwood <JHOPSMC@GMAIL.COM>
2023-12-28 19:50:51 -0600

    Finished movement logic.

--- src/playercharacter.rs
+++ src/playercharacter.rs
@@ -1,27 +1,53 @@
 
 struct Vec2<T> {
     x: T,
-    y: T
+    y: T,
 }
 
 struct PlayerCharacter {
 
-    position: Vec2<i32>, // euclidian coordinates
-    velocity: i32        // m/s
+    position: Vec2<f64>,  // euclidian coordinates in the game world
+    velocity: i32,        // m/s
 }
 
 enum ArrowKeyInput {
     Up,
     Down,
     Left,
-    Right
+    Right,
 }
 
+
+// Use this function to move the PlayerCharacter around in the world accoarding to arrow key input. 
 fn translate_playercharacter(pc: PlayerCharacter, direction: ArrowKeyInput) -> PlayerCharacter {
     match direction {
-        ArrowKeyInput::Up    => pc.position = ,
-        ArrowKeyInput::Down  => return player,
-        ArrowKeyInput::Left  => return player,
-        ArrowKeyInput::Right => return player,
+        ArrowKeyInput::Up    => PlayerCharacter {
+                                    position: Vec2 {
+                                        x: pc.position.x,
+                                        y: pc.position.y + 1 * pc.velocity,
+                                    },
+                                    velocity: pc.velocity
+                                },
+        ArrowKeyInput::Down  => PlayerCharacter {
+                                    position: Vec2 {
+                                        x: pc.position.x,
+                                        y: pc.position.y - 1 * pc.velocity,
+                                    },
+                                    velocity: pc.velocity
+                                },
+        ArrowKeyInput::Left  => PlayerCharacter {
+                                    position: Vec2 {
+                                        x: pc.position.x - 1 * pc.velocity,
+                                        y: pc.position.y, 
+                                    },
+                                    velocity: pc.velocity
+                                },
+        ArrowKeyInput::Right => PlayerCharacter {
+                                    position: Vec2 {
+                                        x: pc.position.x + 1 * pc.velocity,
+                                        y: pc.position.y, 
+                                    },
+                                    velocity: pc.velocity
+                                },
     }
 }
