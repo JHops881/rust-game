a42e70012afb308cdffec08be20a6a93b0e6f709
Joseph Hopwood <JHOPSMC@GMAIL.COM>
2023-12-29 11:46:20 -0600

    added basic text for ui

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,55 @@ async fn main() {
         // Clear screen
         clear_background(BLACK);
         
-        // Sample text
-        let playerpos: String = "(".to_string() + &player.get_position().x.to_string() + ", " + &player.get_position().y.to_string() + ")";
+        // -- Basic Play UI and Debug Information. --
+        let ui_font_size: f32 = 16.0;
+        let new_line: f32 = 18.0;
+        let ui_start_location: f32 = 20.0;
+
+        /*
         
-        draw_text(&playerpos, screen_width() / 2_f32, screen_height() / 2_f32, 12_f32, WHITE);
+        Position: (X, Y)
+        Health: ###
+        Mana: ###
+
+         */
+
+        let player_position_text: String =
+              "Position: (".to_string()
+            + &player.get_position().x.to_string()
+            + ", "
+            + &player.get_position().y.to_string()
+            + ")";
+        let player_health_text: String = 
+              "Health: ".to_string()
+            + &player.get_health().to_string();
+        let player_mana_text: String = 
+              "Mana: ".to_string()
+            + &player.get_mana().to_string();
+
+        draw_text (
+            &player_position_text,
+            ui_start_location,
+            ui_start_location,
+            ui_font_size,
+            WHITE,
+        );
+        draw_text (
+            &player_health_text,
+            ui_start_location,
+            ui_start_location + 1.0 * new_line,
+            ui_font_size,
+            WHITE,
+        );
+        draw_text (
+            &player_mana_text,
+            ui_start_location,
+            ui_start_location + 2.0 * new_line,
+            ui_font_size,
+            WHITE,
+        );
+        // -- End UI --
+
 
         // Allow exiting
         if is_key_down(KeyCode::Escape) { 
