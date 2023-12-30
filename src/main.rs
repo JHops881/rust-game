6235efadb4cf0cfdd27020c3a386e258294a3e90
Joseph Hopwood <JHOPSMC@GMAIL.COM>
2023-12-30 16:46:26 -0600

    Fixed issues

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
-
 use macroquad::prelude::*;
 
-pub mod character;
-use character::*;
+pub mod player_character;
+use player_character::*;
+
+pub mod enemy_character;
+use enemy_character::*;
 
 const TILE_WIDTH: f32 = 64.0; // pixels
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
-
     // initialize the player's character
     let mut player = PlayerCharacter::new();
     // initalize an enemy
-    let mut ghoul = EnemyCharacter::new(EnemyType::Ghoul, Vec2{x: 1.0, y:1.0});
+    let mut ghoul = EnemyCharacter::new(EnemyType::Ghoul, Vec2 { x: 1.0, y: 1.0 });
 
     // Let's throw them in lists to iterate through...
 
-    let mut allies:  Vec<&mut PlayerCharacter> = Vec::new(); // gonna do something with these later.
-    let mut enemies: Vec<&mut EnemyCharacter> = Vec::new();  // Although, not really sure it's the best
-                                                             // way to go about it... Oh Well!
+    let mut allies: Vec<&mut PlayerCharacter> = Vec::new(); // gonna do something with these later.
+    let mut enemies: Vec<&mut EnemyCharacter> = Vec::new(); // Although, not really sure it's the best
+                                                            // way to go about it... Oh Well!
     set_fullscreen(true);
 
     // gme loop crap
@@ -28,7 +29,6 @@ async fn main() {
     let game_time_factor: f32 = 1.0;
 
     loop {
-
         // EXAMPLE GAME LOOP STRUCTURE
         // 1. input
         // 2. update
@@ -37,12 +37,10 @@ async fn main() {
         // delta time calculations
         let real_delta_time = get_time() as f32 - last_update_time;
         last_update_time = last_update_time + real_delta_time;
-        let game_delta_time : f32 = real_delta_time * game_time_factor;
-
-       
+        let game_delta_time: f32 = real_delta_time * game_time_factor;
 
         /* INPUT */
-        // handled for us by macro quad. 
+        // handled for us by macro quad.
 
         /* UPDATE */
         // arrow moevemnt
@@ -60,8 +58,7 @@ async fn main() {
         if is_key_down(KeyCode::S) {
             player.translate(Direction::Down, game_delta_time);
         }
-        
-        // sprinting mechanic implementation from Jason Ryan de la Masa
+
         if is_key_pressed(KeyCode::LeftShift) {
             player.begin_sprint();
         }
@@ -70,7 +67,7 @@ async fn main() {
         }
 
         // Allow exiting
-        if is_key_down(KeyCode::Escape) { 
+        if is_key_down(KeyCode::Escape) {
             std::process::exit(0);
         }
 
@@ -78,26 +75,27 @@ async fn main() {
         // Clear screen
         clear_background(BLACK);
 
-        // draw player 
+        // draw player
         draw_player_character(&player, &player, RED);
 
         // draw enemy
         draw_enemy_character(&ghoul, &player, WHITE);
-        
+
         // Basic Play UI and Debug Information.
         draw_gui(&player);
-      
 
         next_frame().await
     }
 }
 
-
-
 /* Graphics functions. These need to be moved later on or deleted. Although, they work for now. 7:51pm 12/29/2023 */
 // Not my problem!
-/// use to convert the game world position of a character to a position on the screen. 
-pub fn convert_to_screen_coords(world_coords: Vec2, center: &PlayerCharacter, tile_width: f32) -> Vec2 {
+/// use to convert the game world position of a character to a position on the screen.
+pub fn convert_to_screen_coords(
+    world_coords: Vec2,
+    center: &PlayerCharacter,
+    tile_width: f32,
+) -> Vec2 {
     Vec2 {
         x: (world_coords.x - center.get_position().x) * tile_width + screen_width() / 2.0,
         y: (world_coords.y - center.get_position().y) * -1.0 * tile_width + screen_height() / 2.0,
@@ -105,25 +103,41 @@ pub fn convert_to_screen_coords(world_coords: Vec2, center: &PlayerCharacter, ti
 }
 
 // crappy functions that will draw characters as a circles on the screen. Horrendous... Oh Well!
-pub fn draw_player_character(character: &PlayerCharacter, perspective_from: &PlayerCharacter, c: Color) {
-
+pub fn draw_player_character(
+    character: &PlayerCharacter,
+    perspective_from: &PlayerCharacter,
+    c: Color,
+) {
     // get the position of the character as screen coordinates...
-    let character_screen_position: Vec2 = convert_to_screen_coords(character.get_position(), &perspective_from, TILE_WIDTH);
-    // draw call. Self explanitory. 
-    draw_circle(character_screen_position.x, character_screen_position.y, 32.0, c);
-
+    let character_screen_position: Vec2 =
+        convert_to_screen_coords(character.get_position(), &perspective_from, TILE_WIDTH);
+    // draw call. Self explanitory.
+    draw_circle(
+        character_screen_position.x,
+        character_screen_position.y,
+        32.0,
+        c,
+    );
 } // I can't get the generics/templating to work so I just made two different functions, ... Works for now ig. Jeez.
 
-pub fn draw_enemy_character(character: &EnemyCharacter, perspective_from: &PlayerCharacter, c: Color) {
-    let character_screen_position: Vec2 = convert_to_screen_coords(character.get_position(), &perspective_from, TILE_WIDTH);
-    draw_circle(character_screen_position.x, character_screen_position.y, 32.0, c);
+pub fn draw_enemy_character(
+    character: &EnemyCharacter,
+    perspective_from: &PlayerCharacter,
+    c: Color,
+) {
+    let character_screen_position: Vec2 =
+        convert_to_screen_coords(character.get_position(), &perspective_from, TILE_WIDTH);
+    draw_circle(
+        character_screen_position.x,
+        character_screen_position.y,
+        32.0,
+        c,
+    );
 }
 
-
-// Let's put the indev GUI into a single procedure that we can call in the main loop ezpz. 
-// This LGTM! 
+// Let's put the indev GUI into a single procedure that we can call in the main loop ezpz.
+// This LGTM!
 pub fn draw_gui(player: &PlayerCharacter) {
-
     let ui_font_size: f32 = 16.0;
     let new_line: f32 = 18.0;
     let ui_start_location: f32 = 20.0;
@@ -131,43 +145,38 @@ pub fn draw_gui(player: &PlayerCharacter) {
     /* EXAMPLE
 
     \/ [top left of screen] \/
-    
+
     "Position: (X, Y)"
     "Health: ###"
     "Mana: ###"
-    
+
     /\   /\    /\   /\   /\  /\
     */
 
     // This may be unreadable. Just refer to the example above. It does that.
-    let player_position_text: String =
-          "Position: (".to_string()
+    let player_position_text: String = "Position: (".to_string()
         + &player.get_position().x.to_string()
         + ", "
         + &player.get_position().y.to_string()
         + ")";
-    let player_health_text: String = 
-          "Health: ".to_string()
-        + &player.get_health().to_string();
-    let player_mana_text: String = 
-          "Mana: ".to_string()
-        + &player.get_mana().to_string();
+    let player_health_text: String = "Health: ".to_string() + &player.get_health().to_string();
+    let player_mana_text: String = "Mana: ".to_string() + &player.get_mana().to_string();
 
-    draw_text (
+    draw_text(
         &player_position_text,
         ui_start_location,
         ui_start_location,
         ui_font_size,
         WHITE,
     );
-    draw_text (
+    draw_text(
         &player_health_text,
         ui_start_location,
         ui_start_location + 1.0 * new_line,
         ui_font_size,
         WHITE,
     );
-    draw_text (
+    draw_text(
         &player_mana_text,
         ui_start_location,
         ui_start_location + 2.0 * new_line,
---
+++ src/player_character.rs
@@ -0,0 +1,156 @@
+use macroquad::math::Vec2;
+// Vec2 Docs: https://docs.rs/macroquad/latest/macroquad/math/struct.Vec2.html
+
+
+
+pub enum Direction {
+    Right,
+    Left,
+    Up,
+    Down,
+}
+
+// We will get to cast these later. Here are the mana costs used for implementation.
+pub enum Spell {
+    KeneticPulse,
+    Lightning,
+}
+const KENETIC_PULSE_COST: f32 = 10.0;
+const LIGHTNING_COST: f32 = 30.0;
+
+// Star of the show!
+pub struct PlayerCharacter {
+    position: Vec2,    // euclidian coordinates in the game world
+    speed: f32,        // current speed in m/s
+    normal_speed: f32, // jogging speed, just using WASD
+    sprint_speed: f32, // 2x the normal speed, hold shift to access
+
+    is_dead: bool,
+    current_health: f32,
+    max_health: f32,
+
+    is_oom: bool, // (Out Of Mana)
+    current_mana: f32,
+    max_mana: f32,
+}
+
+
+
+const PLAYER_JOG_SPEED:    f32 = 2.68;
+const PLAYER_SPRINT_SPEED: f32 = 2.0 * PLAYER_JOG_SPEED;
+
+impl PlayerCharacter {
+    /// Default Constructor | Get a fresh player character.
+    pub fn new() -> PlayerCharacter {
+        PlayerCharacter {
+            position: Vec2 { x: 0.0, y: 0.0 },
+            speed: PLAYER_JOG_SPEED,
+            normal_speed: PLAYER_JOG_SPEED,
+            sprint_speed: PLAYER_SPRINT_SPEED,
+
+            is_dead: false,
+            current_health: 100.0,
+            max_health: 100.0,
+
+            is_oom: false,
+            current_mana: 100.0,
+            max_mana: 100.0,
+        }
+    }
+    // TODO: A fix is needed. See Trello. Remove this when resolved.
+    /// Construct a Player Character from saved data
+    pub fn from_saved() {}
+
+    /// Use this procedure to move the PlayerCharacter around in the world accoarding to arrow key input.
+    pub fn translate(&mut self, d: Direction, deltat: f32) {
+        match d {
+            Direction::Right => self.position.x = self.position.x + 1.0 * self.speed * deltat,
+            Direction::Left => self.position.x = self.position.x - 1.0 * self.speed * deltat,
+            Direction::Up => self.position.y = self.position.y + 1.0 * self.speed * deltat,
+            Direction::Down => self.position.y = self.position.y - 1.0 * self.speed * deltat,
+        }
+    }
+
+    /// toggles sprinting of player on
+    pub fn begin_sprint(&mut self) {
+        self.speed = self.sprint_speed;
+    }
+    /// toggles sprinting of player off
+    pub fn end_sprint(&mut self) {
+        self.speed = self.normal_speed;
+    }
+
+    pub fn get_position(&self) -> Vec2 {
+        self.position
+    }
+
+    /* --- === ======== === ### Health Functions ### === ======== === --- */
+
+    /// Reduce the health of a player character. This will kill
+    /// the player if it is too much damage.
+    pub fn hurt(&mut self, amount: f32) {
+        if self.current_health - amount > 0.0 {
+            self.current_health = self.current_health - amount;
+        } else {
+            self.current_health = 0.0;
+            self.is_dead = true;
+        }
+    }
+    /// Increase a player character health by amount. Cannot overheal (exceed max hp value)
+    pub fn heal(&mut self, amount: f32) {
+        if self.current_health + amount < self.max_health {
+            self.current_health = self.current_health + amount;
+        } else {
+            self.current_health = self.max_health;
+        }
+    }
+
+    pub fn get_health(&self) -> f32 {
+        self.current_health
+    }
+
+    /* --- === ======== === ### Mana Functions ### === ======== === --- */
+
+    /// Safely reduces the mana of a player character by amount.
+    pub fn drain(&mut self, amount: f32) {
+        if self.current_mana - amount > 0.0 {
+            self.current_mana = self.current_mana - amount;
+        } else {
+            self.current_mana = 0.0;
+            self.is_oom = true;
+        }
+    }
+    /// Safely increases mana of a player character by amount.
+    pub fn energize(&mut self, amount: f32) {
+        if self.current_mana + amount < self.max_mana {
+            self.max_mana = self.max_mana + amount;
+        } else {
+            self.current_mana = self.max_mana;
+        }
+        self.is_oom = false;
+    }
+
+    /// Safely handles an attempt to cast a spell. Returns whether or not is possible
+    pub fn can_cast(&mut self, spell: Spell) -> bool {
+        match spell {
+            Spell::KeneticPulse => {
+                if self.current_mana >= KENETIC_PULSE_COST {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            Spell::Lightning => {
+                if self.current_mana >= LIGHTNING_COST {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    pub fn get_mana(&self) -> f32 {
+        self.current_mana
+    }
+}
