424ab1f884b7f4975c6fa9bb52a2754411dff379
Joshua Hopwood <josh.hopwood@ansys.com>
2023-12-30 12:57:51 -0600

    Temp

--- src/tiles.rs
+++ src/tiles.rs
@@ -5,15 +5,19 @@ static TILE_ATLAS: OnceLock<Texture2D> = OnceLock::new();
 
 /// Load in the tile atlas for use in future calls
 pub fn init_tile_atlas() {
-    TILE_ATLAS.set(Texture2D::from_file_with_format(
+    let atlas = Texture2D::from_file_with_format(
         include_bytes!("../assets/atlas/atlas1.png"),
         Some(ImageFormat::Png),
-    )).expect("Initializing the tile atlas failed.");
-    // build_textures_atlas();
+    );
+    atlas.set_filter(FilterMode::Nearest);
+    TILE_ATLAS
+        .set(atlas)
+        .expect("Initializing the tile atlas failed.");
+    // build_textures_atlas(); // Unsure how this works or if it is even necessary.
 }
 
 /// # Returns
-/// 
+///
 /// The tile atlas
 fn get_tile_atlas() -> &'static Texture2D {
     match TILE_ATLAS.get() {
@@ -33,27 +37,34 @@ impl Tile {
         match self {
             Tile::GroundCenter => (0.0, 0.0),
         }
-    } 
+    }
 }
 
-// Draw a tile to the screen 
+/// Draw a tile to the screen
 pub fn draw_tile(tile: Tile, position: Vec2) {
-    
     const TILE_SIDE_LENGTH: f32 = 16.0;
     const SCALE: f32 = 10.0;
 
     let (x, y) = tile.get_texture_coords();
 
     let draw_params = DrawTextureParams {
-        dest_size: Some(Vec2{ x: TILE_SIDE_LENGTH * SCALE, y: TILE_SIDE_LENGTH * SCALE }),
-        source: Some(Rect{x, y, w: TILE_SIDE_LENGTH, h: TILE_SIDE_LENGTH}),
+        dest_size: Some(Vec2 {
+            x: TILE_SIDE_LENGTH * SCALE,
+            y: TILE_SIDE_LENGTH * SCALE,
+        }),
+        source: Some(Rect {
+            x,
+            y,
+            w: TILE_SIDE_LENGTH,
+            h: TILE_SIDE_LENGTH,
+        }),
         rotation: 0.0,
         flip_x: false,
         flip_y: false,
         pivot: None
     };
 
-    let color = Color::from_rgba(0x0b, 0x66, 0x23, 0xff);
+    let color = Color::from_rgba(0x0b, 0x46, 0x23, 0xff);
 
     draw_texture_ex(get_tile_atlas(), position.x, position.y, color, draw_params);
 }
